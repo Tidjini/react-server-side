53f062422ed452817233e3e3b43b467ff0bd4a62
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-05-08 16:24:39 +0100

    make the express app handle all routes and pass it to react router dom

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static("public"));
 
 //handle the root get request
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.send(renderer(req));
 });
 
