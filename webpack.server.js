7cf7336ff69eb994f28b6dfe97e528b23a40f4f1
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-05-08 15:21:06 +0100

    Ignoring files with webpack (server side)

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base");
+const webpackNodeExternals = require("webpack-node-externals");
 
 const config = {
   //targeting the node js instead the browser we need to transform JSX, and JS in server side
@@ -13,7 +14,9 @@ const config = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build")
-  }
+  },
+
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
