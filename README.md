bb3d4b61dbfa8c06ea343292ca3c890c740bb24a
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-05-13 09:34:57 +0100

    Use Context to send 404 statue

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
     1/ not found page
         first create a not found page then attach it to the routes file without any path field
+    2/ informe the browser with 404 status (if page not found)
+        for this we use the context object inside the static router then pass it to not found page as Static Context props
+        manage the notFound field in here then cominucate with index.js
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -6,10 +6,10 @@ import { StaticRouter } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
 import Routes from "../client/Routes";
 
-export default (req, store) => {
+export default (req, store, context) => {
   const content = renderToString(
     <Provider store={store}>
-      <StaticRouter location={req.path} context={{}}>
+      <StaticRouter location={req.path} context={context}>
         <div>{renderRoutes(Routes)}</div>
       </StaticRouter>
     </Provider>
